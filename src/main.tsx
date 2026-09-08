26e39c395e743ae9e46fba19b55669f3e1e5bbbf
Dragoljub <dracorisz@gmail.com>
2026-09-08 22:49:59 +0200

    feat: initialize shared auth session

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { AuthProvider } from './auth/AuthProvider'
 import './index.css'
 import './media.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>,
 )
