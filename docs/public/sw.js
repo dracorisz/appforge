721adc67f83735b2d46030036f54668c4eb59dee
Dragoljub <dracorisz@gmail.com>
2026-09-10 02:29:10 +0200

    scope Pages cache cleanup to AppForge

--- docs/public/sw.js
+++ docs/public/sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', event => {
   event.waitUntil((async () => {
     const keys = await caches.keys()
-    await Promise.all(keys.map(key => caches.delete(key)))
+    await Promise.all(keys.filter(key => key.toLowerCase().includes('appforge')).map(key => caches.delete(key)))
     await self.registration.unregister()
     const clients = await self.clients.matchAll({ type: 'window', includeUncontrolled: true })
     for (const client of clients) client.navigate(client.url)
