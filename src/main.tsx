7b1bb939a3a3420b6664ba69b7fcfd5692d1777d
Dragoljub <dracorisz@gmail.com>
2026-09-11 02:37:29 +0200

    Expose public blog and changelog routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './auth/AuthProvider'
 import { PwaLifecycle } from './components/pwa/PwaLifecycle'
 import { CookieNotice } from './components/public/CookieNotice'
+import { PublicBlogArticlePage, PublicBlogPage, PublicChangelogPage } from './components/public/PublicContentPages'
 import './index.css'
 import './media.css'
+
+function RootApp() {
+  const location = useLocation()
+
+  if (location.pathname === '/blog') return <PublicBlogPage />
+  if (location.pathname === '/changelog') return <PublicChangelogPage />
+  if (location.pathname.startsWith('/blog/')) {
+    const slug = location.pathname.slice('/blog/'.length).replace(/\/+$/, '')
+    return <PublicBlogArticlePage slug={slug} />
+  }
+
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
     <AuthProvider>
-      <App />
+      <RootApp />
       <CookieNotice />
       <PwaLifecycle />
     </AuthProvider>
