c18e220e2ad94b479a05fe3e767de0166def07a7
Dragoljub <dracorisz@gmail.com>
2026-09-13 18:29:48 +0200

    style: normalize Task List and Crypto spacing

---
+++ src/components/dashboard/CompactAppWrappers.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TaskList as TaskListCore } from './TaskList'
+import { PF_CryptoTrack as CryptoTrackCore } from './PF_CryptoTrack'
+
+export function TaskListCompact() {
+  return <div data-compact-app-stack="task-list"><TaskListCore /></div>
+}
+
+export function CryptoTrackCompact() {
+  return <div data-compact-app-stack="crypto-track"><CryptoTrackCore /></div>
+}
